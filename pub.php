9b0d4e870120210cbe56669f0acb86ed77e8f201
Supapat18 <54792147+Supapat18@users.noreply.github.com>
2019-09-11 15:11:44 +0700

    Update pub.php

--- pub.php
+++ pub.php
@@ -1,8 +1,8 @@
  <?php
  function pubMqtt($topic,$msg){
-       $APPID= "numpapicklinebot/"; //enter your appid
-     $KEY = "mJ7K4MfteC7p0dW"; //enter your key
-    $SECRET = "pp4gzMhCvJIqlxc66hKEvk46m"; //enter your secret
+       $APPID= "Mr16/"; //enter your appid
+     $KEY = "YKmrGr9uoi1kOjk"; //enter your key
+    $SECRET = "ONlB1wDfeYSAygcZlFuoYCIIa"; //enter your secret
     $Topic = "$topic"; 
       put("https://api.netpie.io/microgear/".$APPID.$Topic."?retain&auth=".$KEY.":".$SECRET,$msg);
  
