93b112b20c3f5b9c178dec707f7833068c72e0f0
Supapat18 <54792147+Supapat18@users.noreply.github.com>
2019-09-12 22:58:28 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,7 +1,7 @@
 <?php
 $access_token='84Mb06td1kt1lHte3rfwxVEreoFVDRBnkwsA7Pc0OadL2YJqDNgIwTv4Xqh7l1aVnG3/P6Vgn1DfjgTHho6ypjcszGGpvXtTfE+3/CMyNnuGVB3CGUKw0lyxPu8k1aiBUpVZQTJQL1QSaUQMeO8pnwdB04t89/1O/w1cDnyilFU=';
 
-$url = 'https://api.line.me/v1/oauth/verify';
+$url = 'https://api.line.me/v2/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
